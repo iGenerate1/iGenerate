518ac33ec11b2ecbff9ae4906f2de4a1be004061
iGenerate1 <iGenerate23@gmail.com>
2025-03-27 16:04:17 +0800

    FINAL PERO DI PA FINAL SA HOMEPAGE

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,26 +1,28 @@
 //Clicking of the Button and Carousel Event
+//DOMContentLoaded event is fired when the browser has loaded the HTML, excluding stylesheets, images, and other resources.
 document.addEventListener("DOMContentLoaded", () => {
     const pokeButton = document.getElementById("pokeButton");
     const carouselElement = document.getElementById("heroCarousel");
     const carousel = new bootstrap.Carousel(carouselElement, { interval: false });
     const borderCircle = document.querySelector("#borderSVG circle");
 
-    let clickCount = 0;
+    let clickCount = 1;
     const stepSize = 104; //Each step fills 1/3 of the border
 
     pokeButton.addEventListener("click", () => {
-        carousel.next(); // Move to the next image
+        carousel.next(); //Move to the next image
 
-        if (clickCount < 1000) {
+        if (clickCount > 0) {
             clickCount++;
-            borderCircle.style.strokeDashoffset = `${209 - (clickCount * stepSize)}`;
+            //Update the stroke offset to fill the border
+            borderCircle.style.strokeDashoffset = `${201 - (clickCount * stepSize)}`;
         }
     });
 });
 
 //Active links for the current page
 document.addEventListener("DOMContentLoaded", function () {
-        //Get the current URL path without query parameters
+        //Get the current URL path without parameters
         let currentPath = window.location.pathname.split("/").pop() || "";
 
         //Define page links with corresponding IDs
